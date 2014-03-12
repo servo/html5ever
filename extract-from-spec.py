bd5a80b24a487b9510b17befd3109d8f51cb8595
Keegan McAllister <kmcallister@mozilla.com>
2014-03-12 15:11:21 -0700

    Extract a tokenizer state graph for reference

--- extract-from-spec.py
+++ extract-from-spec.py
@@ -1,28 +1,57 @@
 #!/usr/bin/env python
 import re
-from bs4 import BeautifulSoup
+import bs4
 
 # Extract information from the WHATWG webapp spec.
 
 def parse_spec():
     with file('webapps.html') as f:
-        soup = BeautifulSoup(f)
+        soup = bs4.BeautifulSoup(f)
 
     return {
         'tokenization': soup.find(text='Tokenization').find_parent('div'),
     }
 
+def tokenizer_state_ident(longname):
+    longname = longname.lower()
+    assert longname[-5:] == 'state'
+    words = re.sub(r'[^a-z]', ' ', longname[:-5]).split()
+    return ''.join(w.title() for w in words)
+
 def extract_tokenizer_states(spec):
     with file('tokenizer/states.rs', 'w') as f:
-        f.write('pub enum State {\n');
+        f.write('pub enum State {\n')
 
         for statedefn in spec['tokenization'].select('h5 > dfn'):
-            statename = statedefn.text.lower()
-            assert statename[-5:] == 'state'
-            words = re.sub(r'[^a-z]', ' ', statename[:-5]).split()
-            f.write('    %s,\n' % (''.join(w.title() for w in words),))
+            f.write('    %s,\n' % (tokenizer_state_ident(statedefn.text)))
+
+        f.write('}\n')
+
+def extract_tokenizer_graph(spec):
+    with file('build/states.dot', 'w') as f:
+        f.write('strict digraph {\n')
+
+        for sec in spec['tokenization'].select('h5'):
+            name = sec.text
+            if name == 'Tokenizing character references':
+                continue
+            ident = tokenizer_state_ident(name)
+
+            txt = ''
+            for sib in sec.next_siblings:
+                if isinstance(sib, bs4.Tag):
+                    if sib.name == 'h5':
+                        break
+                    txt += sib.get_text()
+                else:
+                    txt += sib
+
+            for edge in re.finditer(r'[sS]witch to the (.* state)', txt):
+                f.write('    %s -> %s;\n' % (ident, tokenizer_state_ident(edge.group(1))))
 
         f.write('}\n')
 
 spec = parse_spec()
-extract_tokenizer_states(spec)
+
+# extract_tokenizer_states(spec)  # has manual changes
+extract_tokenizer_graph(spec)
