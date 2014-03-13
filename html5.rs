4e5b53f3a0695e2bfa0cb09949a02e6c28eb643d
Keegan McAllister <kmcallister@mozilla.com>
2014-03-13 11:25:11 -0700

    Enhance tokenizer test program

--- html5.rs
+++ html5.rs
@@ -4,18 +4,66 @@
 
 #[feature(macro_rules)];
 
+use std::io;
+use std::char;
+
+use tokenizer::{TokenSink, Token, Tokenizer};
+use tokenizer::{CharacterToken, TagToken, StartTag, EndTag};
+
 pub mod tokenizer;
 
-struct TokenPrinter;
+struct TokenPrinter {
+    in_char_run: bool,
+}
 
-impl tokenizer::TokenSink for TokenPrinter {
-    fn process_token(&mut self, token: tokenizer::Token) {
-        println!("{:?}", token);
+impl TokenPrinter {
+    fn is_char(&mut self, is_char: bool) {
+        match (self.in_char_run, is_char) {
+            (false, true ) => print!("CHAR : "),
+            (true,  false) => println!(""),
+            _ => (),
+        }
+        self.in_char_run = is_char;
+    }
+}
+
+impl TokenSink for TokenPrinter {
+    fn process_token(&mut self, token: Token) {
+        match token {
+            CharacterToken(c) => {
+                self.is_char(true);
+                char::escape_default(c, |d| print!("{:c}", d));
+            }
+            TagToken(tag) => {
+                self.is_char(false);
+                // This is not proper HTML serialization, of course.
+                match tag.kind {
+                    StartTag => print!("TAG  : <\x1b[32m{:s}\x1b[0m", tag.name),
+                    EndTag   => print!("TAG  : <\x1b[31m/{:s}\x1b[0m", tag.name),
+                }
+                for attr in tag.attrs.iter() {
+                    print!(" \x1b[36m{:s}\x1b[0m='\x1b[34m{:s}\x1b[0m'", attr.name, attr.value);
+                }
+                if tag.self_closing {
+                    print!(" \x1b[31m/\x1b[0m");
+                }
+                println!(">");
+            }
+            _ => {
+                self.is_char(false);
+                println!("OTHER: {:?}", token);
+            }
+        }
     }
 }
 
 fn main() {
-    let mut sink = TokenPrinter;
-    let mut tok = tokenizer::Tokenizer::new(&mut sink);
-    tok.feed("<div novalue unquoted=foo singlequoted='bar' doublequoted=\"baz\">Hello, world!</div>");
+    let mut sink = TokenPrinter {
+        in_char_run: false,
+    };
+    {
+        let mut tok = Tokenizer::new(&mut sink);
+        tok.feed(io::stdin().read_to_str());
+    }
+    sink.is_char(false);
 }
