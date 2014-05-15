4d9732c652aa872c03cb4ca057fba5e674a0e884
Keegan McAllister <kmcallister@mozilla.com>
2014-05-14 17:31:21 -0700

    More preliminary tree builder work

--- src/tree_builder/mod.rs
+++ src/tree_builder/mod.rs
@@ -2,8 +2,71 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// FIXME
+#![allow(unused_imports)]
+
 pub use self::interface::{QuirksMode, Quirks, LimitedQuirks, NoQuirks};
 pub use self::interface::TreeSink;
 
+use tokenizer::{Doctype, Attribute, AttrName, TagKind, StartTag, EndTag, Tag};
+use tokenizer::{Token, DoctypeToken, TagToken, CommentToken};
+use tokenizer::{CharacterToken, MultiCharacterToken, EOFToken, ParseError};
+use tokenizer::TokenSink;
+
+use std::default::Default;
+
 mod interface;
 mod states;
+
+/// Tree builder options, with an impl for Default.
+#[deriving(Clone)]
+pub struct TreeBuilderOpts {
+   pub scripting_enabled: bool,
+}
+
+impl Default for TreeBuilderOpts {
+    fn default() -> TreeBuilderOpts {
+        TreeBuilderOpts {
+            scripting_enabled: true,
+        }
+    }
+}
+
+pub struct TreeBuilder<'sink, Handle, Sink> {
+    /// Options controlling the behavior of the tree builder.
+    opts: TreeBuilderOpts,
+
+    /// Consumer of tree modifications.
+    sink: &'sink mut Sink,
+
+    /// Insertion mode.
+    mode: states::InsertionMode,
+
+    /// Original insertion mode, used by Text and InTableText modes.
+    orig_mode: states::InsertionMode,
+
+    /// Stack of open elements, most recently added at end.
+    open_elems: Vec<Handle>,
+
+    /// Head element pointer.
+    head_elem: Option<Handle>,
+}
+
+impl<'sink, Handle, Sink: TreeSink<Handle>> TreeBuilder<'sink, Handle, Sink> {
+    pub fn new(sink: &'sink mut Sink, opts: TreeBuilderOpts) -> TreeBuilder<'sink, Handle, Sink> {
+        TreeBuilder {
+            opts: opts,
+            sink: sink,
+            mode: states::Initial,
+            orig_mode: states::Initial,
+            open_elems: vec!(),
+            head_elem: None,
+        }
+    }
+}
+
+impl<'sink, Handle, Sink: TreeSink<Handle>> TokenSink for TreeBuilder<'sink, Handle, Sink> {
+    fn process_token(&mut self, _token: Token) {
+        fail!("not implemented!");
+    }
+}
