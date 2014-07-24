168ca5e270932514609f75dccd3bb87cffebe9f7
Keegan McAllister <kmcallister@mozilla.com>
2014-07-24 11:44:38 -0700

    Add a no-op tree builder example

---
+++ examples/noop-tree-builder.rs
@@ -0,0 +1,67 @@
+// Copyright 2014 The html5ever Project Developers. See the
+// COPYRIGHT file at the top-level directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+extern crate debug;
+
+extern crate html5ever;
+
+use std::io;
+use std::default::Default;
+use std::string::String;
+use std::collections::hashmap::HashMap;
+
+use html5ever::{Namespace, Atom, parse_to, one_input};
+use html5ever::tokenizer::Attribute;
+use html5ever::tree_builder::{TreeSink, QuirksMode};
+
+struct Sink {
+    next_id: uint,
+    names: HashMap<uint, (Namespace, Atom)>,
+}
+
+impl TreeSink<uint> for Sink {
+    fn get_document(&mut self) -> uint {
+        0
+    }
+
+    fn same_node(&self, x: uint, y: uint) -> bool {
+        x == y
+    }
+
+    fn elem_name(&self, target: uint) -> (Namespace, Atom) {
+        self.names.find(&target).expect("not an element").clone()
+    }
+
+    fn create_element(&mut self, ns: Namespace, name: Atom, _attrs: Vec<Attribute>) -> uint {
+        let id = self.next_id;
+        self.next_id += 1;
+        self.names.insert(id, (ns, name));
+        id
+    }
+
+    fn parse_error(&mut self, _msg: String) { }
+    fn set_quirks_mode(&mut self, _mode: QuirksMode) { }
+    fn append_text(&mut self, _parent: uint, _text: String) { }
+    fn append_comment(&mut self, _parent: uint, _text: String) { }
+    fn append_element(&mut self, _parent: uint, _child: uint) { }
+    fn append_doctype_to_document(&mut self, _name: String, _public_id: String, _system_id: String) { }
+    fn add_attrs_if_missing(&mut self, _target: uint, _attrs: Vec<Attribute>) { }
+    fn remove_from_parent(&mut self, _target: uint) { }
+    fn mark_script_already_started(&mut self, _node: uint) { }
+}
+
+fn main() {
+    let mut sink = Sink {
+        next_id: 1,
+        names: HashMap::new(),
+    };
+
+    let input = io::stdin().read_to_str().unwrap();
+    parse_to(&mut sink, one_input(input), Default::default());
+}
