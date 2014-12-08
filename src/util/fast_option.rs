f97b6475dd4cc85020c037d7c0785e151117adee
Clark Gaebel <cgaebel@mozilla.com>
2014-12-08 14:04:34 -0800

    license headers

--- src/util/fast_option.rs
+++ src/util/fast_option.rs
@@ -1,3 +1,12 @@
+// Copyright 2014 The html5ever Project Developers. See the
+// COPYRIGHT file at the top-level directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 //! Options of big things (like `Option<SingleChar>`) make rustc generate really
 //! bad code with lots of moves. When rustc gets non-lexical borrows and NRVO,
 //! this module will be unnecessary. In the mean time, this is an unsafe option
