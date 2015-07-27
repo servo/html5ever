d8c698a9b74eda4d6d0097bd1f17b7e601847b04
Simon Sapin <simon.sapin@exyr.org>
2015-07-27 13:24:36 +0200

    Have Travis-CI also test on stable Rust

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -10,11 +10,20 @@
 
 set -ex
 
+# Test without unstable first, to make sure src/tree_builder/rules.expanded.rs is up-to-date.
 cargo test --no-run
 cargo test | ./scripts/shrink-test-output.py
 r=${PIPESTATUS[0]}
 if [ $r -ne 0 ]; then exit $r; fi
 
-cargo test --manifest-path capi/Cargo.toml
+if [ $TRAVIS_RUST_VERSION = nightly ]
+then
+    cargo test --no-run --features unstable
+    cargo test --features unstable | ./scripts/shrink-test-output.py
+    r=${PIPESTATUS[0]}
+    if [ $r -ne 0 ]; then exit $r; fi
+
+    cargo test --manifest-path capi/Cargo.toml
+fi
 
 cargo doc
