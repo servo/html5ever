dbcd1e571b4686c3769ba2588691c04b81977ae1
Daniel Fath <daniel.fath7@gmail.com>
2016-01-28 23:09:20 +0100

    Add example for simple DOM access

---
+++ examples/hello_xml.rs
@@ -0,0 +1,49 @@
+#!/usr/bin/env run-cargo-script
+//! This is a regular crate doc comment, but it also contains a partial
+//! Cargo manifest.  Note the use of a *fenced* code block, and the
+//! `cargo` "language".
+//!
+//! ```cargo
+//! [dependencies]
+//! xml5ever = "0.1.1"
+//! tendril = "0.1.3"
+//! ```
+extern crate xml5ever;
+extern crate tendril;
+
+use std::default::Default;
+
+use tendril::{SliceExt};
+use xml5ever::{parse, one_input};
+use xml5ever::rcdom::{RcDom, NodeEnum, Text};
+
+fn main() {
+    // Using SliceExt.to_tendril functions we can read stdin
+    let input = "<hello>XML</hello>".to_tendril();
+
+    // To parse XML into a tree form, we need a TreeSink
+    // luckily xml5ever comes with a static RC backed tree represetation.
+    let dom: RcDom = parse(one_input(input), Default::default());
+
+    // Do some processing
+    let doc = &dom.document;
+
+    let hello_node = &doc.borrow().children[0];
+    let hello_tag = &*dom.elem_name(hello_node).local;
+    let text_node = &hello_node.borrow().children[0];
+
+    let xml = {
+        let mut xml = String::new();
+
+        match &text_node.borrow().node {
+            &Text(ref text) => {
+                xml.push_str(&*text);
+            },
+            e => {println!("{:?}", e);},
+        };
+
+        xml
+    };
+
+    println!("{:?} {:?}!", hello_tag, xml);
+}
