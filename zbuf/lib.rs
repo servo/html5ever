6469ca0e809c61e5ded67cd0b4902695426665cc
Simon Sapin <simon.sapin@exyr.org>
2017-05-11 14:12:13 +0200

    Conversion functions are private to the crate.

--- zbuf/lib.rs
+++ zbuf/lib.rs
@@ -11,13 +11,13 @@ pub use strbuf::StrBuf;
 
 #[cfg(any(target_pointer_width = "32",
           target_pointer_width = "64"))]
-pub fn u32_to_usize(x: u32) -> usize {
+fn u32_to_usize(x: u32) -> usize {
     x as usize  // Valid because usize is at least as big as u32
 }
 
 #[cfg(any(target_pointer_width = "32",
           target_pointer_width = "64"))]
-pub fn usize_to_u32(x: usize) -> u32 {
+fn usize_to_u32(x: usize) -> u32 {
     const MAX: usize = ::std::u32::MAX as usize;  // Valid because usize is at least as big as u32
     assert!(x <= MAX, "overflow");
     x as u32
