9a2c7e0096bd46d58005a194f5437a1c61f623c9
Ygg01 <daniel.fath7@gmail.com>
2018-12-12 21:33:32 +0100

    Fix bench settings

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -12,11 +12,11 @@ set -ex
 
 if [ $TRAVIS_RUST_VERSION = nightly ]
 then
-    cargo test -p html5ever --features "rustc-test/capture" --bench tokenizer
+    cargo bench --all
     cargo test -p html5ever --features "rustc-test/capture"
     cargo test -p xml5ever --features "rustc-test/capture"
 else
-    cargo test -p html5ever --bench tokenizer
+    cargo bench --all
     cargo test --all
 fi
 
