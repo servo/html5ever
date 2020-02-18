aed68aa105607ee7fb2f979c9b5120a16d4f116f
Simon Sapin <simon.sapin@exyr.org>
2020-02-18 13:55:00 +0100

    Use `TryFrom`

--- zbuf/lib.rs
+++ zbuf/lib.rs
@@ -50,9 +50,6 @@ fn u32_to_usize(x: u32) -> usize {
     x as usize // Valid because usize is at least as big as u32
 }
 
-#[cfg(any(target_pointer_width = "32", target_pointer_width = "64"))]
 fn usize_to_u32(x: usize) -> u32 {
-    const MAX: usize = ::std::u32::MAX as usize; // Valid because usize is at least as big as u32
-    assert!(x <= MAX, "overflow");
-    x as u32
+    std::convert::TryFrom::try_from(x).unwrap_or_else(|_| panic!("overflow"))
 }
