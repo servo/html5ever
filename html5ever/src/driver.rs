032de9f1464a01b2321dda6d7b9710c52256c2a6
Sven Neumann <sven@svenfoo.org>
2021-01-21 12:17:25 +0100

    Suppress clippy warnings about method names that violate conventions
    Changing these method names would break the public API, so I guess that is not
    an option.
    
    https://rust-lang.github.io/rust-clippy/master/index.html#wrong_self_convention

--- html5ever/src/driver.rs
+++ html5ever/src/driver.rs
@@ -130,6 +130,7 @@ impl<Sink: TreeSink> Parser<Sink> {
     ///
     /// Use this when your input is bytes that are known to be in the UTF-8 encoding.
     /// Decoding is lossy, like `String::from_utf8_lossy`.
+    #[allow(clippy::wrong_self_convention)]
     pub fn from_utf8(self) -> Utf8LossyDecoder<Self> {
         Utf8LossyDecoder::new(self)
     }
