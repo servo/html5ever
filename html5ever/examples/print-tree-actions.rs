9637d89dcd8f62c94698f35dd7acfd23e7757479
Victor Porton <porton@narod.ru>
2021-07-25 12:26:15 +0300

    no(api_v2) tests passed

--- html5ever/examples/print-tree-actions.rs
+++ html5ever/examples/print-tree-actions.rs
@@ -155,9 +155,15 @@ impl TreeSink for Sink {
         println!("Set current line to {}", line_number);
     }
 
+    #[cfg(api_v2)]
     fn pop(&mut self, elem: &usize) -> Result<(), SuperfluousClosingElement> {
         println!("Popped element {}", elem);
         Ok(())
+    }
+
+    #[cfg(not(api_v2))]
+    fn pop(&mut self, elem: &usize) {
+        println!("Popped element {}", elem);
     }
 }
 
