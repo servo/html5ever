ff62f5941e753f12a8688dd46b1c45bb783e4238
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2022-01-21 20:01:21 +0100

    wip

--- html5ever/src/tokenizer/mod.rs
+++ html5ever/src/tokenizer/mod.rs
@@ -1129,7 +1129,7 @@ impl<Sink: TokenSink> Tokenizer<Sink> {
                     '\0' => go!(self: error; append_comment "--\u{fffd}"; to Comment),
                     '!' => go!(self: error; to CommentEndBang),
                     '-' => go!(self: error; push_comment '-'),
-                    c => go!(self: error; append_comment "--"; push_comment c; to Comment),
+                    c => go!(self: append_comment "--"; reconsume Comment),
                 }
             },
 
